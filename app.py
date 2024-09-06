613751779304beb3f9fc78d5c132f0fcdf9f90ae
Chaitanya Aluru <chaitanya.aluru@gmail.com>
2024-09-06 00:33:37 -0700

    support multiple stocks

--- app.py
+++ app.py
@@ -10,35 +10,42 @@ sns.set()
 ui.panel_title("Finance Bro")
 
 # Input for stock symbol and price how many days ago
-ui.input_text("ticker_val", "Ticker Symbol", "TXG")
-ui.input_numeric("days_ago", "days ago", 30)
-data = yf.download("TXG", period="5y")
+ui.input_text("ticker_val", "Ticker Symbol", "aapl, nvda")
+ui.input_numeric("days_ago", "Number of Days", 30)
 
 
 @render.text
 def text_val():
     """Loads stock data and displays stock price."""
-    global data
-    data = yf.download(input.ticker_val(), period="5y")
-    stock_data = data["Close"]
+    all_symbols = [i.strip() for i in input.ticker_val().split(",")]
+    all_data = []
+    for symbol in all_symbols:
+        all_data.append(yf.download(symbol, period="5y"))
+    stock_data = all_data[0]["Close"]
     stock_price = stock_data[-1 - int(input.days_ago())]
-    return (
-        f"Closing  Price {input.days_ago()} Trading Days Ago: ${round(stock_price, 2)}"
-    )
+    return f"Closing  Price of {str.upper(all_symbols[0])} {input.days_ago()} Trading Days Ago: ${round(stock_price, 2)}"
 
 
 @render.plot
 def stock_chart():
     """Creates stock chart."""
-    stock_data = list(data["Close"])
-    stock_name = yf.Ticker(input.ticker_val()).info["longName"]
-    stock_values = stock_data[-1 - input.days_ago() :]
-    plt.plot(range(len(stock_values)), stock_values)
-    plt.title(f"{stock_name} Stock Price")
+    all_symbols = [i.strip() for i in input.ticker_val().split(",")]
+    all_data = []
+    for symbol in all_symbols:
+        all_data.append(yf.download(symbol, period="5y"))
+    plt.figure()
+    stock_names = []
+    for symbol, data in zip(all_symbols, all_data):
+        stock_data = list(data["Close"])
+        stock_names.append(yf.Ticker(symbol).info["longName"])
+        stock_values = stock_data[-1 - input.days_ago() :]
+        plt.plot(range(len(stock_values)), stock_values, label=symbol)
+    plt.title(f"Stock Chart for {' and '.join(stock_names)}")
+    plt.legend()
     step = max(int(input.days_ago() / 15), 1)
-    stock_dates = [i.strftime("%Y-%m-%d") for i in data.index[-1 - input.days_ago() :]][
-        ::step
-    ]
+    stock_dates = [
+        i.strftime("%Y-%m-%d") for i in all_data[0].index[-1 - input.days_ago() :]
+    ][::step]
     plt.xticks(ticks=range(0, len(stock_values), step), labels=stock_dates, rotation=45)
     plt.xlabel("Date")
     plt.ylabel("Closing Price ($)")
