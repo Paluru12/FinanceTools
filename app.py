a222785b66621f97605860249918aca42b45d593
Chaitanya Aluru <chaitanya.aluru@gmail.com>
2024-09-06 00:03:36 -0700

    formatting

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
-import yfinance as yf
-from shiny.express import input, render, ui
+# Shiny app for displaying stock info
+
 import matplotlib.pyplot as plt
 import seaborn as sns
+import yfinance as yf
+from shiny.express import input, render, ui
+
 sns.set()
 
 ui.panel_title("Finance Bro")
 
-#Input for stock symbol and price how many days ago
+# Input for stock symbol and price how many days ago
 ui.input_text("ticker_val", "Ticker Symbol", "TXG")
 ui.input_numeric("days_ago", "days ago", 30)
 data = yf.download("TXG", period="5y")
 
+
 @render.text
 def text_val():
+    """Loads stock data and displays stock price."""
     global data
     data = yf.download(input.ticker_val(), period="5y")
-    stock_data = data['Close']
-    stock_price = stock_data[-1-int(input.days_ago())]
-    return f"Closing  Price {input.days_ago()} Trading Days Ago: ${round(stock_price, 2)}"
+    stock_data = data["Close"]
+    stock_price = stock_data[-1 - int(input.days_ago())]
+    return (
+        f"Closing  Price {input.days_ago()} Trading Days Ago: ${round(stock_price, 2)}"
+    )
+
 
 @render.plot
 def stock_chart():
-    stock_data = list(data['Close'])
-    stock_name = yf.Ticker(input.ticker_val()).info['longName']
-    stock_values = stock_data[-1-input.days_ago():]
+    """Creates stock chart."""
+    stock_data = list(data["Close"])
+    stock_name = yf.Ticker(input.ticker_val()).info["longName"]
+    stock_values = stock_data[-1 - input.days_ago() :]
     plt.plot(range(len(stock_values)), stock_values)
     plt.title(f"{stock_name} Stock Price")
     step = max(int(input.days_ago() / 15), 1)
-    stock_dates = [i.strftime("%Y-%m-%d") for i in data.index[-1-input.days_ago():]][::step]
+    stock_dates = [i.strftime("%Y-%m-%d") for i in data.index[-1 - input.days_ago() :]][
+        ::step
+    ]
     plt.xticks(ticks=range(0, len(stock_values), step), labels=stock_dates, rotation=45)
     plt.xlabel("Date")
     plt.ylabel("Closing Price ($)")
